bc2da2dafaecb98eccaf3c6d1e0e3ae517bfb5c7
bakka22 <abobakereltom@gmail.com>
2024-06-04 22:22:52 +0200

    Task #8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,9 +2,18 @@ export default function cleanSet(set, startString) {
   if (startString === '') {
     return '';
   }
+  let flag = false;
   let newString = '';
   for (const s of set) {
-    if (s.startsWith(startString)) {
+    for (let i = 0; i < startString.length; i += 1) {
+      if (s[i] === startString[i]) {
+        flag = true;
+      } else {
+        flag = false;
+        break;
+      }
+    }
+    if (flag) {
       newString += s.substring(startString.length);
       newString += '-';
     }
