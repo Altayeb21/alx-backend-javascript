fa93fa00a07a2c878317bee61918253f9c4e0ec4
bakka22 <abobakereltom@gmail.com>
2024-06-04 14:22:25 +0200

    s

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,8 @@
 export default function getListStudents() {
   const list = [];
-  const ob1 = { id: 1, name: 'Guillaume', location: 'San Francisco' };
-  const ob2 = { id: 2, name: 'James', location: 'Columbia' };
-  const ob3 = { id: 5, name: 'Serena', location: 'San Francisco' };
+  const ob1 = { id: 1, firstname: 'Guillaume', location: 'San Francisco' };
+  const ob2 = { id: 2, firstname: 'James', location: 'Columbia' };
+  const ob3 = { id: 5, firstname: 'Serena', location: 'San Francisco' };
   list.push(ob1, ob2, ob3);
   return list;
 }
