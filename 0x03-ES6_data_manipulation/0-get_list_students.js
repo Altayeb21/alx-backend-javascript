35886f8f63c25dce55f54d5b8604eb18180605fc
bakka22 <abobakereltom@gmail.com>
2024-06-04 14:19:39 +0200

    s

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,10 +1,8 @@
-
 export default function getListStudents() {
-	
-	let list = [];
-	let ob1 = {id: 1, name: "Guillaume", location: "San Francisco"};
-	let ob2 = {id: 2, name: "James", location: "Columbia"};
-	let ob3 = {id: 5, name: "Serena", location: "San Francisco"};
-	list.push(ob1, ob2, ob3);
-	return list;
+  const list = [];
+  const ob1 = { id: 1, name: 'Guillaume', location: 'San Francisco' };
+  const ob2 = { id: 2, name: 'James', location: 'Columbia' };
+  const ob3 = { id: 5, name: 'Serena', location: 'San Francisco' };
+  list.push(ob1, ob2, ob3);
+  return list;
 }
