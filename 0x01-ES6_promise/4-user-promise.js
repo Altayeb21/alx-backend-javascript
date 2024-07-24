799ef0e58cc3a834c6c662e9aebc8c2e9de55981
bakka22 <abobakereltom@gmail.com>
2024-07-24 13:23:08 +0200

    s

---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,7 @@
+export default function signUpUser(firstName, lastName) {
+  const promise = new Promise((resolve) => {
+    const obj = { firstName, lastName };
+    resolve(obj);
+  });
+  return promise;
+}
