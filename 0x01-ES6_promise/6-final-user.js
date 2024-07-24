8ef54f2ceb02169de710409829cea18d4289e686
bakka22 <135711566+bakka22@users.noreply.github.com>
2024-07-24 23:45:45 +0200

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,15 +2,12 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const lst = [{}, {}];
-  const promise1 = signUpUser(firstName, lastName);
-  const promise2 = uploadPhoto(fileName);
-  return promise1.then((response) => {
-    lst[0].status = promise1;
-    lst[0].value = response;
-    return promise2;
-  }).catch((error) => {
-    lst[1].status = promise2;
-    lst[1].value = error;
-  }).then(() => lst);
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((response) => (
+      response.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }
