808029a9ff3dc7011a26d71c803982a637c5477e
bakka22 <abobakereltom@gmail.com>
2024-07-30 05:26:00 +0200

    s

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -4,6 +4,7 @@ export default function createInt8TypedArray(length, position, value) {
   }
   const buffer = new ArrayBuffer(length);
   const int8 = new Int8Array(buffer);
+  const dv = new DataView(buffer);
   int8[position] = value;
-  return buffer;
+  return dv;
 }
