b155be4b275058f15173f5930173a273ad4df6db
bakka22 <abobakereltom@gmail.com>
2024-07-30 05:36:09 +0200

    s

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,4 +1,4 @@
-export default function createInt8TypedArray(length, position, value) {
+/* export default function createInt8TypedArray(length, position, value) {
   if (position > length) {
     throw new Error('Position outside range');
   }
@@ -7,4 +7,13 @@ export default function createInt8TypedArray(length, position, value) {
   const dv = new DataView(buffer);
   int8[position] = value;
   return dv;
-}
+} */
+const createInt8TypedArray = (length, position, value) => {
+  const buffer = new ArrayBuffer(length);
+  const int8Array = new Int8Array(buffer);
+  if (position > int8Array.length) throw new Error('Position outside range');
+  int8Array[position] = value;
+  return new DataView(buffer);
+};
+
+export default createInt8TypedArray;
