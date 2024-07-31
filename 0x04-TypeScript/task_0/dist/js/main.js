dfc5a0ad2c00b89a6926ddd268a2f02e6f32840b
bakka22 <abobakereltom@gmail.com>
2024-07-31 21:15:34 +0200

    S

---
+++ 0x04-TypeScript/task_0/dist/js/main.js
@@ -0,0 +1,14 @@
+var st1 = {
+    firstName: 'bakka',
+    lastName: 'saleem',
+    age: 25,
+    location: 'cairo',
+};
+var st2 = {
+    firstName: 'tom',
+    lastName: 'saleem',
+    age: 65,
+    location: 'cairo',
+};
+var studentsList = [st1, st2];
+//# sourceMappingURL=main.js.map
---
+++ 0x04-TypeScript/task_0/js/main.js
@@ -0,0 +1,13 @@
+var st1 = {
+    firstName: 'bakka',
+    lastName: 'saleem',
+    age: 25,
+    location: 'cairo'
+};
+var st2 = {
+    firstName: 'tom',
+    lastName: 'saleem',
+    age: 65,
+    location: 'cairo'
+};
+var studentsList = [st1, st2];
