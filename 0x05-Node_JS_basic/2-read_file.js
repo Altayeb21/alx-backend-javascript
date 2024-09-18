33e563d88097d174d8fee14b3da66beb85ec8783
bakka22 <abobakereltom@gmail.com>
2024-09-18 06:20:41 +0200

    S

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+
+/* eslint-disable prefer-destructuring */
+/* eslint-disable guard-for-in */
+function countStudents (path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    let numberOfStudents = 0;
+    const dataDict = {};
+    const dataArray = data.split('\n').slice(1);
+    for (const chunk of dataArray) {
+      const student = chunk.split(',');
+      if (student.length < 4) {
+        /* eslint-disable no-continue */
+        continue;
+        /* eslint-enable no-continue */
+      }
+      numberOfStudents += 1;
+      if (student[3] in dataDict) {
+        dataDict[student[3]][1][dataDict[student[3]][0]] = student[0];
+        dataDict[student[3]][0] += 1;
+      } else {
+        dataDict[student[3]] = [];
+        dataDict[student[3]][0] = 1;
+        dataDict[student[3]][1] = [];
+        dataDict[student[3]][1][0] = student[0];
+      }
+    }
+    console.log(`Number of students: ${numberOfStudents}`);
+    for (const key in dataDict) {
+      process.stdout.write(`Number of students in ${key}: ${dataDict[key][0]}. List: `);
+      for (const name of dataDict[key][1]) {
+        if (name !== dataDict[key][1][dataDict[key][1].length - 1]) {
+          process.stdout.write(`${name}, `);
+        } else {
+          process.stdout.write(`${name}\n`);
+        }
+      }
+    }
+  } catch (err) {
+    console.log('Cannot load the database');
+  }
+}
+/* eslint-enable prefer-destructuring */
+/* eslint-enable guard-for-in */
+
+module.exports = countStudents;
